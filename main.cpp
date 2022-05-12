c0880e2ea6490242309889168ff93037bf774a01
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-12 11:22:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
 int main(){
+    char lista[10][20];
+    int selezione = 0;
+    int num = 0;
+    while (true) {
+        cout << "1: inserimento " << endl;
+        cout << "2: stampa " << endl;
+        cout << "6: exit " << endl;
+        cin >> selezione;
+        switch (selezione) {
+            case 1:
+                inserimento(lista, num);
+                break;
+            case 2:
+                //stampa(lista);
+                break;
+            case 3:
+                return 0;
+            default:
+                cout << "inculati" << endl;
+        }
+    }
   return 0;
 }
